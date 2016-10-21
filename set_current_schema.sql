db26ae3da646d6d2058d464d2d2702adcd0632b9
Stefan Oravec <stefan.oravec@me.com>
2016-10-21 11:08:13 +0200

    fix get/set current schema when not connected

--- set_current_schema.sql
+++ set_current_schema.sql
@@ -1,6 +1,6 @@
-define g_current_schema = &1
+define l_current_schema = &1
 
-alter session set current_schema = &&g_current_schema;
+alter session set current_schema = &&l_current_schema;
 
 column current_schema new_value g_current_schema
 set termout off
@@ -8,3 +8,5 @@ select sys_context('userenv','current_schema') as current_schema from dual;
 set termout on
 
 prompt Current schema changed to &&g_current_schema
+
+undefine l_current_schema
