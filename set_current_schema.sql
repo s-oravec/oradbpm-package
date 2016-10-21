9dcfe9ec13de5154973f3d6288c7fcc22c767c4d
Stefan Oravec <stefan.oravec@me.com>
2016-10-21 10:32:36 +0200

    get/set current schema

---
+++ set_current_schema.sql
@@ -0,0 +1,10 @@
+define g_current_schema = &1
+
+alter session set current_schema = &&g_current_schema;
+
+column current_schema new_value g_current_schema
+set termout off
+select sys_context('userenv','current_schema') as current_schema from dual;
+set termout on
+
+prompt Current schema changed to &&g_current_schema
