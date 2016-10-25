90ee1af20b985fd52af5ba188537618848e1c741
Stefan Oravec <stefan.oravec@me.com>
2016-10-25 10:59:56 +0200

    grants and synonyms for API types

--- module/api/unset_dependency_ref_owner.sql
+++ module/api/unset_dependency_ref_owner.sql
@@ -1,2 +1,14 @@
 prompt .. Dropping synonym INVOICER
 drop synonym invoicer;
+
+prompt .. Dropping synonym INVOICER_INVOICE
+drop synonym invoicer_invoice;
+
+prompt .. Dropping synonym INVOICER_CONTACT
+drop synonym invoicer_contact;
+
+prompt .. Dropping synonym INVOICER_INVOICE_ITEM
+drop synonym invoicer_invoice_item;
+
+prompt .. Dropping synonym INVOICER_INVOICE_ITEMS
+drop synonym invoicer_invoice_items;
