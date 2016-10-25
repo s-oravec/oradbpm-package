473c802dfc4d591f16c26c0b603821f6be94b16b
Stefan Oravec <stefan.oravec@me.com>
2016-10-25 10:59:56 +0200

    grants and synonyms for API types
    (cherry picked from commit 90ee1af)

--- module/api/grant_public.sql
+++ module/api/grant_public.sql
@@ -1,2 +1,14 @@
 prompt .. Granting EXECUTE on INVOICER package to PUBLIC
 grant execute on invoicer to public;
+
+prompt .. Granting EXECUTE on INVOICER_INVOICE type to PUBLIC
+grant execute on invoicer_invoice to public;
+
+prompt .. Granting EXECUTE on INVOICER_CONTACT type to PUBLIC
+grant execute on invoicer_contact to public;
+
+prompt .. Granting EXECUTE on INVOICER_INVOICE_ITEM type to PUBLIC
+grant execute on invoicer_invoice_item to public;
+
+prompt .. Granting EXECUTE on INVOICER_INVOICE_ITEMS type to PUBLIC
+grant execute on invoicer_invoice_items to public;
--- module/api/set_dependency_ref_owner.sql
+++ module/api/set_dependency_ref_owner.sql
@@ -1,2 +1,14 @@
 prompt .. Creating synonym INVOICER for &&g_schema_name..INVOICER
 create synonym invoicer for &&g_schema_name..invoicer;
+
+prompt .. Creating synonym INVOICER_INVOICE for &&g_schema_name..INVOICER_INVOICE
+create synonym invoicer_invoice for &&g_schema_name..invoicer_invoice ;
+
+prompt .. Creating synonym INVOICER_CONTACT for &&g_schema_name..INVOICER_CONTACT
+create synonym invoicer_contact for &&g_schema_name..invoicer_contact ;
+
+prompt .. Creating synonym INVOICER_INVOICE_ITEM for &&g_schema_name..INVOICER_INVOICE_ITEM
+create synonym invoicer_invoice_item for &&g_schema_name..invoicer_invoice_item ;
+
+prompt .. Creating synonym INVOICER_INVOICE_ITEMS for &&g_schema_name..INVOICER_INVOICE_ITEMS
+create synonym invoicer_invoice_items for &&g_schema_name..invoicer_invoice_items ;
